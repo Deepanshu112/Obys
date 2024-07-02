5d49e6bacfc8b29d7b6156c874d870dbca4d3784
Deepanshu112 <140751260+Deepanshu112@users.noreply.github.com>
2024-07-02 23:37:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
+//locomotive js
 function locomotiveAnimation() {
   gsap.registerPlugin(ScrollTrigger);
-
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
   });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
 
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length
@@ -25,18 +22,17 @@ function locomotiveAnimation() {
         height: window.innerHeight,
       };
     },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    
     pinType: document.querySelector("#main").style.transform
       ? "transform"
       : "fixed",
   });
 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 }
+
 function loadingAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1", {
@@ -92,6 +88,8 @@ function loadingAnimation() {
     "-=1.2"
   );
 }
+
+
 function cursorAnimation() {
   Shery.mouseFollower({
     skew: true,
